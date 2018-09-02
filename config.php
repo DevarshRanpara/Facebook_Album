5f152aeb0a6720218a6c924682f2c3fd8b00f71c
DevarshRanpara <devarshranpara@hotmail.com>
2018-09-02 11:23:02 +0530

    Save

--- config.php
+++ config.php
@@ -10,8 +10,8 @@
     require_once "lib/Facebook/autoload.php";
 
     $FB = new \Facebook\Facebook([
-        'app_id' => '1161329700689969',
-        'app_secret' => 'b2ab8339d351b74b45bb4c61e45b8585',
+        'app_id' => '1161329700689969',                         // Facebook App ID
+        'app_secret' => 'b2ab8339d351b74b45bb4c61e45b8585',     // Facebook App Secret key
         'default_graph_version' => 'v3.1'
     ]);
 
